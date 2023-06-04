9f25a8cd5a3a2d1a322dd74a81c81814f3f212b1
Yaakov Haimoff <hyaakov@aol.com>
2023-06-04 18:16:00 +0300

    git init

---
+++ include/macros.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <functional>
+#include <vector>
+#include <set>
+
+using std::map;
+using std::vector;
+using std::pair;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::ifstream;
+using std::multimap;
+using std::abs;
+using std::set;
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "macros.h"
+
+int main() {
+
+    return 0;
+}
